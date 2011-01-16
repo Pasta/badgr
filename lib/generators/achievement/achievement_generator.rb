270c938198f4038b2e07e3be8e58b9d418720b89
Vincent Coste <coste.vincent@gmail.com>
2011-01-16 21:15:15 +0100

    migration bug

--- lib/generators/achievement/achievement_generator.rb
+++ lib/generators/achievement/achievement_generator.rb
@@ -2,7 +2,7 @@ class AchievementGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('../templates', __FILE__)
   
   def generate_achievement
-     template "achievement.rb", "app/models/achievements/#{name.underscore}.rb"
+     template "achievement.rb", "app/models/achievements/#{name.underscore}_achievement.rb"
      template "achievement_observer.rb", "app/models/achievements/#{name.underscore}_observer.rb"
      template "achievement_test.rb", "test/unit/achievements/#{name.underscore}_test.rb"
   end
