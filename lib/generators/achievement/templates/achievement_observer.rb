6f50727e7dce483cd68c06ff8d7b4afd3aa36493
Vincent Coste <coste.vincent@gmail.com>
2011-01-16 22:12:18 +0100

    generator bug into observer

--- lib/generators/achievement/templates/achievement_observer.rb
+++ lib/generators/achievement/templates/achievement_observer.rb
@@ -2,6 +2,6 @@ class <%= name.camelize %>AchievementObserver < ActiveRecord::Observer
   observe :some_model
   
   def after_create(some_model)
-    <%= name.camelize %>Achievement.award_achievements_for(some_model.user) if ('your conditions here')
+    <%= name.camelize %>Achievement.award_achievement_for(some_model.user) if ('your conditions here')
   end
 end
