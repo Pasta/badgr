8dcc007ac9adca4b5f95adf45c498e2e607f55af
Vincent Coste <coste.vincent@gmail.com>
2011-01-18 13:01:34 +0100

    mixin rails way

--- app/models/achievements.rb
+++ app/models/achievements.rb
@@ -1,19 +1,28 @@
 module Achievements
+  extend ActiveSupport::Concern
   
-  def self.included(base)
-    base.class_eval do
-      has_many :achievements
+  
+  included do
+    has_many :achievements
+  end
+  
+  
+  # def self.included(base)
+  #     base.class_eval do
+  #       
+  #     end
+  #   end
+  
+  
+     def award_with(achievement, options = {})
+      achievement.create!(:user => self, :level => options[:level])
     end
-  end
   
-  def award_with(achievement, options = {})
-    achievement.create!(:user => self, :level => options[:level])
-  end
   
-  def has_achievement?(achievement, level = nil)
-    conditions = {:type => achievement.to_s, :user_id => self.id}    
-    conditions[:level] = level if level
-    achievements.first(:conditions => conditions).present?
-  end
-  
+
+    def has_achievement?(achievement, level = nil)
+      conditions = {:type => achievement.to_s, :user_id => self.id}    
+      conditions[:level] = level if level
+      achievements.first(:conditions => conditions).present?
+    end
 end
